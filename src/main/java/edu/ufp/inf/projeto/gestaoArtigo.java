2758143af7375399516b45c83290dbe31192f9c3
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 23:00:58 +0100

    Update gestaoArtigo.java

--- src/main/java/edu/ufp/inf/projeto/gestaoArtigo.java
+++ src/main/java/edu/ufp/inf/projeto/gestaoArtigo.java
@@ -1,4 +1,15 @@
 package edu.ufp.inf.projeto;
 
 public interface gestaoArtigo {
+  
+  void addArtigo(Double key,String value);
+
+void addArtigoNode(double key,String value);
+
+void deleteArtigo(double key);
+  
+Node deleteArtigoNode(Node h, double key);
+  
+Iterable<Double> getArtigo();
+  
 }
