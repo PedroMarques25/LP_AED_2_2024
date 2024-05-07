7ee881c869eb7aa0b856b01f805d3922196fe9cd
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 23:01:34 +0100

    Update gestaoConferencia.java

--- src/main/java/edu/ufp/inf/projeto/gestaoConferencia.java
+++ src/main/java/edu/ufp/inf/projeto/gestaoConferencia.java
@@ -1,4 +1,15 @@
 package edu.ufp.inf.projeto;
 
 public interface gestaoConferencia {
+
+void addConferencia(Double key,String value);
+
+void addConferenciaNode(double key,String value);
+
+void deleteConferencia(double key);
+  
+Node deleteConferenciaNode(Node h, double key);
+  
+Iterable<Double> getConferencias();
+  
 }
