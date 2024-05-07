139a5f9ae4c4c1163eca76895b38ba53076fbe9d
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 10:48:14 +0100

    Update gestãoRedBlack.java

--- src/main/java/edu/ufp/inf/projeto/gestãoRedBlack.java
+++ src/main/java/edu/ufp/inf/projeto/gestãoRedBlack.java
@@ -1,9 +1,17 @@
 package edu.ufp.inf.projeto;
 
+
+import org.w3c.dom.Node;
+
 public interface gestãoRedBlack {
 
 void addDataRB(Double key,String value);
 
 void addNode(double key,String value);
+Node put(Node h, double key, String value);
+
+void delete(double key);
+Node delete(Node h, double key);
+
 
 }
