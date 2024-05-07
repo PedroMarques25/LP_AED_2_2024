76d56555a1d5873b034ba4713fd0be9e5692ee47
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 10:48:46 +0100

    Update BD.java

--- src/main/java/edu/ufp/inf/projeto/BD.java
+++ src/main/java/edu/ufp/inf/projeto/BD.java
@@ -4,14 +4,14 @@ import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.ST;
+import org.w3c.dom.Node;
+
+import java.util.List;
 
 public class BD implements gestãoSTI,gestãoRedBlack{
 
   ST<Double, String> st = new ST();
   RedBlackBST<Double, String> devices = new RedBlackBST();
-
-
-
 
   public Integer ArrayList;
 
@@ -23,39 +23,44 @@ public class BD implements gestãoSTI,gestãoRedBlack{
   public void addKeyST(Double key, String value) {
     st.put(key,value);
   }
-
   @Override
   public String getKey(Double key) {
     return st.get(key);
   }
-
   @Override
   public void removeKey(Double key) {
     st.remove(key);
   }
-
   @Override
   public boolean containsKey(Double key) {
     return st.contains(key);
   }
-
   @Override
   public int getSize() {
     return st.size();
   }
-
   @Override
   public Iterable<Double> getKeys() {
     return st.keys();
   }
-
   @Override
   public void addDataRB(Double key, String value) {
     devices.put(key, value);
   }
-
   @Override
   public void addNode(double key,String value) {
 
+  }
+  @Override
+  public org.w3c.dom.Node put(org.w3c.dom.Node h, double key, String value) {
+    return h;
+  }
+  @Override
+  public void delete(double key) {
+     devices.delete(key);
+  }
+  @Override
+  public Node delete(Node h, double key) {
+    return null;
   }
 }
