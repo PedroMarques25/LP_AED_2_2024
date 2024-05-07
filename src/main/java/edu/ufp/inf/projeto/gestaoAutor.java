2839ae3da1e98c509ff0018ef46526c191860174
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 11:39:54 +0100

    Update gestaoAutor.java

--- src/main/java/edu/ufp/inf/projeto/gestaoAutor.java
+++ src/main/java/edu/ufp/inf/projeto/gestaoAutor.java
@@ -1,4 +1,16 @@
 package edu.ufp.inf.projeto;
+import org.w3c.dom.Node;
 
 public interface gestaoAutor {
+  
+void addAutor(Double key,String value);
+
+void addAutorNode(double key,String value);
+
+void deleteAutor(double key);
+  
+Node deleteAutorNode(Node h, double key);
+  
+Iterable<Double> getAutores();
+
 }
