4fbb03ba9cb5c7b362ecb33d351eb00b3511f19c
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-07 23:02:27 +0100

    Update gestaoJournal.java

--- src/main/java/edu/ufp/inf/projeto/gestaoJournal.java
+++ src/main/java/edu/ufp/inf/projeto/gestaoJournal.java
@@ -1,4 +1,14 @@
 package edu.ufp.inf.projeto;
 
 public interface gestaoJournal {
+
+void addJournal(Double key,String value);
+
+void addJournalNode(double key,String value);
+
+void deleteJournal(double key);
+  
+Node deleteJournalNode(Node h, double key);
+  
+Iterable<Double> getJournais();
 }
