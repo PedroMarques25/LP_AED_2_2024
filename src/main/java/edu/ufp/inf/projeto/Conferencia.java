eba28da0983b7d5798939e84406708ff8d01e01c
PedroMonteiro <118840774+Dedder46@users.noreply.github.com>
2024-05-09 17:29:00 +0100

    Update Conferencia.java

--- src/main/java/edu/ufp/inf/projeto/Conferencia.java
+++ src/main/java/edu/ufp/inf/projeto/Conferencia.java
@@ -1,6 +1,9 @@
 package edu.ufp.inf.projeto;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 public class Conferencia extends LocalPublicacao {
 
   public int num_Edicao;
@@ -10,6 +13,8 @@ public class Conferencia extends LocalPublicacao {
   public Date dataInicio;
 
   public Date dateFim;
+  public List<Artigo> artigos;
+  public List<Conferencia> conferencias = new ArrayList<>();
 
   public void adicionarConferencia(int num_Edicao, String local, Date dateInicio, Date dateFim) {
   }
